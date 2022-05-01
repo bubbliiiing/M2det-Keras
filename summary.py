0f801ac3d712bde0f5278ae11280b41ee728efca
Bubbliiiing <3323290568@qq.com>
2022-05-01 13:53:32 +0800

    update flops and training steps

--- summary.py
+++ summary.py
@@ -2,13 +2,21 @@
 #   该部分代码用于看网络结构
 #--------------------------------------------#
 from nets.m2det import m2det
+from utils.utils import net_flops
 
 if __name__ == "__main__":
-    input_shape = [320, 320, 3]
+    input_shape = [320, 320]
     num_classes = 21
 
-    model = m2det(input_shape, num_classes)
+    model = m2det([input_shape[0], input_shape[1], 3], num_classes)
+    #--------------------------------------------#
+    #   查看网络结构网络结构
+    #--------------------------------------------#
     model.summary()
+    #--------------------------------------------#
+    #   计算网络的FLOPS
+    #--------------------------------------------#
+    net_flops(model, table=False)
 
     # for i,layer in enumerate(model.layers):
     #     print(i,layer.name)
